604bd07a07f04ccaec41981282c3d0cac96a5195
Sitch196 <sichinava1996@gmail.com>
2024-01-20 18:51:32 +0400

    fixing stuff

--- src/components/courses/CoursesSection.jsx
+++ src/components/courses/CoursesSection.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "./CoursesSection.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import ios from "../../../assets/courses/ios.png";
+import react from "../../../assets/courses/react.png";
+import python from "../../../assets/courses/python.png";
+import cyber from "../../../assets/courses/cyber.png";
+import training from "../../../assets/courses/training.png";
+import blockchain from "../../../assets/courses/blockchain.png";
+import devops from "../../../assets/courses/devops.png";
+import security from "../../../assets/courses/ios.png";
 
 export default function CoursesSection() {
   const courses = [
@@ -9,55 +17,55 @@ export default function CoursesSection() {
       id: 1,
       title: "iOS Development",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/ios.png",
+      image: ios,
     },
     {
       id: 2,
       title: "React",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/react.png",
+      image: react,
     },
     {
       id: 3,
       title: "Intro to Python",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/python.png",
+      image: python,
     },
     {
       id: 4,
       title: "Advanced Python",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/python.png",
+      image: python,
     },
     {
       id: 5,
       title: "Advanced Cybersecurity Course",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/cyber.png",
+      image: cyber,
     },
     {
       id: 6,
       title: " ToT - Training of Trainers",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/training.png",
+      image: training,
     },
     {
       id: 7,
       title: "Blockchain",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/blockchain.png",
+      image: blockchain,
     },
     {
       id: 8,
       title: "DevOps",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/devops.png",
+      image: devops,
     },
     {
       id: 9,
       title: "Information Security Governance",
       description: "რეგისტრაცია დასრულებულია",
-      image: "/assets/courses/security.png",
+      image: security,
     },
   ];
 
